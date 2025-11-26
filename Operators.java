9a011217edc36e0b839d126fa8bb06f7078501ab
sabaripriyan10 <sabaripriyan10@gamil.com>
2025-11-26 11:03:25 +0530

    Operators

---
+++ Operators.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Operators{
+	public static void main(String[] args){
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		System.out.println("a = "+a+", b = "+b);
+		System.out.println("Addition: "+(a+b));
+		System.out.println("Subtraction: "+(a-b));
+		System.out.println("Multiplication: "+(a*b));
+		System.out.println("Division: "+(a/b));
+		System.out.println("a > b : "+(a>b));
+		System.out.println("a == b : "+(a==b));
+	}
+}
