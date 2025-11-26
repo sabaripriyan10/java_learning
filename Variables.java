98a4c8eacc7b8b448fa3152349e70648470814dc
sabaripriyan10 <sabaripriyan10@gamil.com>
2025-11-26 09:37:22 +0530

    Variables and Data types

---
+++ Variables.java
@@ -0,0 +1,12 @@
+public class Variables{
+	public static void main(String[] args){
+		String title="Iron Man";
+		float rating=7.9f;
+		short year=2008;
+		boolean hit=true;
+		System.out.println("Movie Title: "+title);
+		System.out.println("Rating: "+rating);
+		System.out.println("Released in: "+year);
+		System.out.println("Is Hit? "+hit);
+	}
+}
